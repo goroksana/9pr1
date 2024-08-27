946a5a75c62984eff713da60c52209cda5dbe093
goroksana <goroksana.gmail.com>
2024-08-26 20:30:57 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GreetComponent } from './greet/greet.component';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,GreetComponent,NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'prNine';
+  greeting = 'Oksana';
+
+  names=[{name: "Inna"}, {name:"Olga"}, {name:"Olena"}];
+
 }
---
+++ src/app/greet/greet.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-greet',
+  standalone: true,
+  imports: [],
+  templateUrl: './greet.component.html',
+  styleUrl: './greet.component.css'
+})
+export class GreetComponent {
+  @Input() name:string="";
+
+
+}
